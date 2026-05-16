8c4caaa0ae5a7169e104e4901fe6a8fb088206c3
Ly Hong Phat <lyhongphat261202@gmail.com>
2026-05-16 20:47:38 +0700

    refactor: rename index.php to main.php and add .vercel to gitignore

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 // Vercel serverless function entry point
-// Forwards the request to the main application index.php
-require __DIR__ . '/../index.php';
+// Forwards the request to the main application main.php
+require __DIR__ . '/../main.php';
